6a5b692d7106b466a925d10b9bcc7a128bcd1ac8
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-04 19:36:55 -0300

    Correção do ex02

--- ex02.py
+++ ex02.py
@@ -10,6 +10,30 @@
     - Milhares
 '''
 
+
+# Solicita um número ao usuário. Para este código funcionar,
+# o usuário DEVE digitar um número de 4 dígitos (ex: 0001, 0123, 1234).
+numero_str = input("Digite um número de 4 dígitos (ex: 0123): ")
+
+# Acessa cada caractere da string diretamente por seu índice
+# A contagem do índice começa do 0.
+milhares = numero_str[0]
+centenas = numero_str[1]
+dezenas = numero_str[2]
+unidades = numero_str[3]
+
+# Exibe o resultado
+print(f"O número digitado foi {numero_str}.")
+print(f"Milhares: {milhares}")
+print(f"Centenas: {centenas}")
+print(f"Dezenas: {dezenas}")
+print(f"Unidades: {unidades}")
+
+
+
+
+
+'''
 num = str(input("digite um numero de 0 a 9999 (com 4 casas): ")) #Lê o Número que o usuário inseriu
 caracteres = list(num) # separa os caracteres
 # Exibição
@@ -17,14 +41,4 @@ print(f"Unidade: {caracteres[3]}")
 print(f"Dezena: {caracteres[2]}")
 print(f"Centenas: {caracteres[1]}")
 print(f"Milhares: {caracteres[0]}")
-
-'''
-num = str(input("digite um numero de 0 a 9999 (com 4 casas): "))
-caracteres = list(num)
-caracteres2 = "".join(caracteres)
-print(f"Unidade: {caracteres2[3]}")
-print(f"Dezena: {caracteres2[2]}")
-print(f"Centenas: {caracteres2[1]}")
-print(f"Milhares: {caracteres2[0]}")
-
 '''
